2f34fbd43c164a723b6bbbdf0202d30c35ca2a47
knsakib <knsakib@gmail.com>
2020-03-13 19:13:27 -0400

    mongo in env

--- models/todoModel.js
+++ models/todoModel.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
+require('dotenv').config();
 //connect to the database
-mongoose.connect('YOUR_MONGODB_STRING', { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(`${process.env.MONGODB_STRING}`, { useNewUrlParser: true, useUnifiedTopology: true })
 
 .then(result => {
     console.log("successfully connected to database");
